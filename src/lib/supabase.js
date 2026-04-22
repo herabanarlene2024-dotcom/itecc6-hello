3a7913771e94ad4ecf06d336d65fa373933d85bd
Arlene Heraban <herabanarlene2024@gmail.com>
2026-04-22 16:42:12 +0800

    feat: add Supabase client configuration

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'ggswymzukvoogpopgahv'
+const supabaseUrl = 'https://ggswymzukvoogpopgahv.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdnc3d5bXp1a3Zvb2dwb3BnYWh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4MzkzMDgsImV4cCI6MjA5MjQxNTMwOH0.V2a1idq5SZvFco-j6Ia_FQfgGEeOlE7RFlMqRsE7-3w'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
