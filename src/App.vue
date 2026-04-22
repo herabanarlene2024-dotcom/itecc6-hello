102c209c891a771b0a25c1abd4081a4aeacbb059
Arlene Heraban <herabanarlene2024@gmail.com>
2026-04-22 16:37:32 +0800

    feat: initial Vue.js Hello Worldsetup

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <h1>{{ message }}</h1>
+    <p>Welcome to ITECC6!</p>
+    <p>Created by: Arlene Heraban</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello from Vue.js!')
+</script>
+
+<style scoped>
+.app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  padding: 2rem;
+}
+</style>
